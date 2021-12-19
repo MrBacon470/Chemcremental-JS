8c7dd8bd25bf6638d19c357da888ef781780de5b
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2021-12-19 15:34:08 -0600

    Changing math ugh

--- JS/Elements.js
+++ JS/Elements.js
@@ -3,20 +3,20 @@ const elementScale = [D(1.10), D(1.15), D(1.20), D(1.25), D(1.35), D(1.40), D(1.
 let elementCost = []
 
 for(let i = 0; i < elementBase.length; i++) {
-    elementCost[i] = elementBase[i] * (Math.pow(elementScale[i], data.elements[i].level))
+    elementCost[i] = elementBase[i].times(Decimal.pow(elementScale[i], data.elements[i].level))
 }
 
 function purchaseElement(i) {
     if(i == 0 || i == 1) {
         if(data.elements[0].amt >= elementCost[i]) {
-            data.elements[i].level++
-            data.elements[0].amt -= elementCost[i]
+            data.elements[i].level.add(1)
+            data.elements[0].amt.minus(elementCost[i])
         }
     }
     else {
         if(data.elements[i - 1].amt >= elementCost[i]) {
-            data.elements[i].level++
-            data.elements[i - 1].amt -= elementCost[i]
+            data.elements[i].level.add(1)
+            data.elements[i - 1].amt.minus(elementCost[i])
         }
     }
 }
