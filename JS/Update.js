279ca720ce53f288f65e1676d520c4d31fcebf96
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2021-12-19 13:54:16 -0600

    Adding Amounts and boost totals

--- JS/Update.js
+++ JS/Update.js
@@ -1,6 +1,7 @@
 // region element declarations
 const elementButtons = [];
 const elementNames = ['Hydrogen','Carbon','Oxygen','Fluorine','Sulfur','Chlorine','Iron','Lead']
+const shortElement = ['H','C','O','F','S','C','Fe','Pb']
 for (let i=0; i<8; i++){
     elementButtons[i] = document.getElementById(`${elementNames[i]}`)
 }
@@ -10,10 +11,13 @@ const tabs = [];
 function updateHTML(){
     if (data.currentTab === 1){
         for(let i = 0;i < 8;i++) {
-            if(i == 0 || i == 1)
-                elementButtons[i].innerHTML = `${data.elements[i].name}  Generator<br>Cost: 10 Hydrogen | Level:0`
+            if(i == 0)
+                elementButtons[i].innerHTML = `${data.elements[i].name}  Generator (${format(data.elements[i].amt)} ${shortElement[i]})<br>Cost: 10 Hydrogen | Level:0`
+            else if(i == 1)
+            elementButtons[i].innerHTML = `${data.elements[i].name}  Generator (${format(data.elements[i].amt)} ${shortElement[i]} | 1.00x)<br>Cost: 10 Hydrogen | Level:0`
+
             else
-                elementButtons[i].innerHTML = `${data.elements[i].name}  Generator<br>Cost: 10 ${data.elements[i - 1].name} | Level:0`
+                elementButtons[i].innerHTML = `${data.elements[i].name}  Generator (${format(data.elements[i].amt)} ${shortElement[i]} | 1.00x)<br>Cost: 10 ${data.elements[i - 1].name} | Level:0`
         }
         
     }
